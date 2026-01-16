e34b79d24d7b415caa2e91b4438334537a81b9ac
22LWOLF <142831758+22LWOLF@users.noreply.github.com>
2026-01-16 10:49:18 -0600

    Coordinates and Setting Map Size
    Can now (in code) give Longitude and latitude to display a map at that position. Can also change the display area of that specified location.

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -12,9 +12,16 @@ import CoreLocation
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    //need to display a map of a designated size and a designated Longitude and Latitude.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //EX Cords Kansas City
+        let cordinates = CLLocationCoordinate2D(latitude: 39.0522, longitude: -94.0553)
+        let region = MKCoordinateRegion(center: cordinates, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(region, animated: true)
+        
     }
 
 
