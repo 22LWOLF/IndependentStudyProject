a767be8ae86dc030cbb85b805d3995edd1d17ad6
22LWOLF <142831758+22LWOLF@users.noreply.github.com>
2026-01-16 10:11:36 -0600

    Added App
    Added the start of the actual file for the app.

---
+++ MapApp/MapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Wolf,Luke D on 1/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
