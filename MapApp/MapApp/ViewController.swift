3b166cd567427955d78e741d09c5ad70967c0597
22LWOLF <142831758+22LWOLF@users.noreply.github.com>
2026-01-20 14:08:37 -0600

    Started point to point route generation
    Has stuff for detecting a tap on the map and inserting up too 2 points. Partial logic to generate a route between them, and partial code for resetting the route if you tap again after making a route.

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    private var selectedCoordinates: [CLLocationCoordinate2D] = []
+    private var currentRoute : MKRoute?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         
         //EX Cords Milan MO (1st value + is North - is South, 2nd value + is East - is West)
         let cordinates = CLLocationCoordinate2D(latitude: 40.2022, longitude: -93.1252)
@@ -22,8 +26,56 @@ class ViewController: UIViewController {
         let region = MKCoordinateRegion(center: cordinates, latitudinalMeters: 10000, longitudinalMeters: 10000)
         mapView.setRegion(region, animated: true)
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(_:)))
+        mapView.addGestureRecognizer(tap)
+        
+        
     }
+    
+    @objc private func handleMapTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        let point = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        selectedCoordinates.append(coordinate)
+        
+        //if route already exists and the user taps again this resets it for a new route
+        if currentRoute != nil || selectedCoordinates.count > 2{
+            resetRouteAnnotations()
+        }
+        
+        //drop a pin
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = selectedCoordinates.count == 1 ? "Start" : "End"
+        mapView.addAnnotation(annotation)
+        
+        //if you have 2 points request a route
+        if selectedCoordinates.count == 2{
+            requestRoute(from: selectedCoordinates[0], to: selectedCoordinates[1])
+        }
+        
+        private func resetRouteAnnotations(){
+            //This will clear the current state of the map and will add the new overlays/annotations
+            //set selectedCoordinates[0] = nothing then populate it with the new tap information
+            //then reset selectedCoordinates[1] to nothing
+            
+            //clear in-memory state
+            selectedCoordinates.removeAll()
+            currentRoute = nil
+            
+            //remove any existing route lines
+            mapView.removeOverlays(mapView.overlays)
+            
+            //remove pins, but keep the user location if available
+            let pinsToRemove = mapView.annotations.filter { !($0 is MKUserLocation) }
+            mapView.removeAnnotations(pinsToRemove)
+            
+        }
+        private func requestRoute(from start: CLLocationCoordinate2D, to end: CLLocationCoordinate2D){
+            // This will build the directions request bewteen the 2 points and draw them
+        }
 
+    
 
+    }
 }
 
