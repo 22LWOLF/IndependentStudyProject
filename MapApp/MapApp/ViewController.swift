7428c0900a7a588593e89312e2c3c9e7afebfa52
22LWOLF <142831758+22LWOLF@users.noreply.github.com>
2026-01-20 10:08:29 -0600

    Now Usable on physical device
    I can now download a version of the app onto my Iphone for testing. I also altered coordinates to see if info would transfer over while still plugged in. It does not.

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -17,8 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //EX Cords Kansas City
-        let cordinates = CLLocationCoordinate2D(latitude: 39.0522, longitude: -94.0553)
+        //EX Cords Milan MO (1st value + is North - is South, 2nd value + is East - is West)
+        let cordinates = CLLocationCoordinate2D(latitude: 40.2022, longitude: -93.1252)
+        //Region is the displayed area on launch of the specified coordinates. (This is a 10 kilometers x 10 kilometers)
         let region = MKCoordinateRegion(center: cordinates, latitudinalMeters: 10000, longitudinalMeters: 10000)
         mapView.setRegion(region, animated: true)
         
